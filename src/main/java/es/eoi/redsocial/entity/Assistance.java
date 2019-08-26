d95b21e2ba191ce2fd484f7ce297b3c6bddbf4a3
carlitosalli <carlitosalvarezlinares@gmail.com>
2019-08-26 17:52:44 +0200

    Add userRepo

--- src/main/java/es/eoi/redsocial/entity/Assistance.java
+++ src/main/java/es/eoi/redsocial/entity/Assistance.java
@@ -2,6 +2,9 @@ package es.eoi.redsocial.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 import lombok.Getter;
@@ -12,6 +15,11 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Assistance {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
 	
 	@Column(name = "idUser")
 	private int idUser;
