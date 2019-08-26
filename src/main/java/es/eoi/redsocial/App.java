6ded65904e812ed081cba5ce23663929f0be40d9
JavieseN <sirjaviern@gmail.com>
2019-08-26 19:40:55 +0200

    Solución de errores(Funciona la creación de la BBDD)

--- src/main/java/es/eoi/redsocial/App.java
+++ src/main/java/es/eoi/redsocial/App.java
@@ -1,17 +1,16 @@
 package es.eoi.redsocial;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.es.eoi")
-@EntityScan( basePackages = {"com.es.eoi"} )
-@ComponentScan(basePackages = "com.es.eoi")
-@EnableAutoConfiguration
+@EnableJpaRepositories(basePackages = "es.eoi.redsocial")
+@EntityScan( basePackages = {"es.eoi.redsocial"} )
+@ComponentScan(basePackages = "es.eoi.redsocial")
+
 public class App 
 {
 	public static void main(String[] args) {
