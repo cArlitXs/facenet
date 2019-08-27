9ad2f990a230c0a311f140a364918f0744175c92
kike <kikegonzalez103@gmail.com>
2019-08-27 19:56:55 +0200

    merging

--- src/main/java/es/eoi/redsocial/service/MessageService.java
+++ src/main/java/es/eoi/redsocial/service/MessageService.java
@@ -1,5 +1,22 @@
 package es.eoi.redsocial.service;
 
+import java.util.List;
+
+import es.eoi.redsocial.entity.Message;
+
+
 public interface MessageService {
 
+	
+	public void save(Message message);
+
+	
+	public List<Message> findAll();
+
+	
+	public Message findById(int id);
+
+	
+	public void remove(int id);
+	
 }
